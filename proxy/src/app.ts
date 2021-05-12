72983739726296acc2a925d3a5d66d5aa17a3803
progfay <shun1856shun@gmail.com>
2021-05-12 23:20:57 +0900

    feat: write delayed proxy

--- proxy/src/app.ts
+++ proxy/src/app.ts
@@ -1,5 +1,27 @@
+import http from "http";
+import httpProxy from "http-proxy";
+
 async function main() {
-  console.log("main");
+  const proxy = httpProxy.createProxyServer();
+  const server = http.createServer(async (req, res) => {
+    const _res = new http.ServerResponse(req);
+    proxy.web(req, _res, {
+      changeOrigin: true,
+      target: "https://www.google.com",
+    });
+    let data: any = "";
+    _res.on("pipe", (readable) => {
+      readable.on("data", (chunk) => {
+        data += chunk;
+      });
+    });
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    res.writeHead(_res.statusCode, _res.statusMessage, _res.getHeaders());
+    res.write(data);
+    res.end();
+  });
+
+  server.listen(3000);
 }
 
 main();
